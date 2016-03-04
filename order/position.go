55f2fb0dfe7f6a12af0a1a7dc3686bc8f9615218
Florian Schlegel <florian.schlegel@bestbytes.com>
2016-03-04 16:19:43 +0100

    removed field ID

--- order/position.go
+++ order/position.go
@@ -2,12 +2,13 @@ package order
 
 // Position in an order
 type Position struct {
-	ID           string
+	//ID           string
 	ItemID       string
 	Name         string
 	Description  string
 	Quantity     float64
 	QuantityUnit string
 	Price        float64
+	IsATPApplied bool
 	Custom       interface{}
 }
