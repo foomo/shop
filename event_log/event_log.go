f4e92ae07769d3c322a54bb6e125f92977609503
Florian Schlegel <florian.schlegel@bestbytes.com>
2016-03-10 19:02:32 +0100

    minor refactoring

--- event_log/event_log.go
+++ event_log/event_log.go
@@ -4,9 +4,12 @@ import (
 	"encoding/json"
 	"log"
 	"time"
+
+	"gopkg.in/mgo.v2/bson"
 )
 
 type Event struct {
+	ID             bson.ObjectId `bson:"_id,omitempty"`
 	Type           EventType
 	Action         string
 	OrderID        string
@@ -33,21 +36,23 @@ const (
 type ActionShop string
 
 const (
-	ActionInsertingOrder   ActionShop = "actionInsertingOrder"
-	ActionUpsertingOrder   ActionShop = "actionUpsertingOrder"
-	ActionCreateOrder      ActionShop = "actionCreatingOrder"
-	ActionDropCollection   ActionShop = "actionDropCollection"
-	ActionValidate         ActionShop = "actionValidate"
-	ActionRetrieveOrder    ActionShop = "actionRetrieveOrder"
-	ActionStatusUpdate     ActionShop = "actionStatusUpdate"
-	ActionApplyATPResponse ActionShop = "actionApplyATPResponse"
-	ActionSendATPRequest   ActionShop = "actionSendATPRequest"
-	ActionSendOrder        ActionShop = "actionSendOrder"
+	ActionInsertingOrder      ActionShop = "actionInsertingOrder"
+	ActionUpsertingOrder      ActionShop = "actionUpsertingOrder"
+	ActionCreateOrder         ActionShop = "actionCreatingOrder"
+	ActionDropEventCollection ActionShop = "actionDropEventCollection"
+	ActionDropAllCollections  ActionShop = "actionDropAllCollections"
+	ActionValidate            ActionShop = "actionValidate"
+	ActionRetrieveOrder       ActionShop = "actionRetrieveOrder"
+	ActionStatusUpdate        ActionShop = "actionStatusUpdate"
+	ActionApplyATPResponse    ActionShop = "actionApplyATPResponse"
+	ActionSendATPRequest      ActionShop = "actionSendATPRequest"
+	ActionSendOrder           ActionShop = "actionSendOrder"
 )
 
+// EventHistory is a field of Order
 type EventHistory []*Event
 
-func (eh *EventHistory) Report() string {
+func (eh EventHistory) Report() string {
 	jsonBytes, err := json.MarshalIndent(eh, "", "	")
 	if err != nil {
 		log.Println("Could not parse json")
@@ -75,4 +80,46 @@ func Debug(i ...interface{}) {
 	if VERBOSE {
 		log.Println("[DEBUG]", i)
 	}
+}
+
+// Print all shop errors in console
+func ReportShopErrors() {
+	p := GetEventPersistor()
+
+	event := &Event{}
+	q := p.GetCollection().Find(&bson.M{"type": EventTypeError})
+	iter := q.Iter()
+	var errCount int
+	for iter.Next(event) {
+		errCount++
+		jsonBytes, err := json.MarshalIndent(event, "", "	")
+		if err != nil {
+			panic(err)
+		}
+		log.Println(string(jsonBytes))
+	}
+	log.Println("Errors:", errCount)
+}
+
+// TODO does not work, errors do not show up
+// Print all Errors on Orders in console
+func ReportOrderErrors() {
+	p := GetEventPersistor()
+
+	eventHistory := &EventHistory{}
+	q := p.GetCollection().Find(&bson.M{}).Select(bson.M{"eventhistory": true})
+	iter := q.Iter()
+	var errCount int
+	for iter.Next(eventHistory) {
+		if len(*eventHistory) > 0 {
+			errCount++
+			jsonBytes, err := json.MarshalIndent(eventHistory, "", "	")
+			if err != nil {
+				panic(err)
+			}
+			log.Println(string(jsonBytes))
+		}
+	}
+	log.Println("Errors:", errCount)
+
 }
