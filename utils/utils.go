f0e9d4af33c5703914789aa1b9c6e21e069455b1
Florian Schlegel <florian.schlegel@bestbytes.com>
2016-03-11 17:41:53 +0100

    DropAllCollections() no longer panics on error. Error is legitimate if database does not exsit and does not hurt further actions.

--- utils/utils.go
+++ utils/utils.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"log"
+
 	"github.com/foomo/shop/event_log"
 	"github.com/foomo/shop/order"
 )
@@ -9,11 +11,13 @@ import (
 func DropAllCollections() {
 	err := event_log.GetEventPersistor().GetCollection().DropCollection()
 	if err != nil {
-		panic(err)
+		// Do not panic here. If db does not yet exist, it is ok for DropCollection to fail.
+		log.Println("Error: EventPersistor DropCollection() ", err)
 	}
 	err = order.GetOrderPersistor().GetCollection().DropCollection()
 	if err != nil {
-		panic(err)
+		// Do not panic here. If db does not yet exist, it is ok for DropCollection to fail.
+		log.Println("Error: EventPersistor DropCollection() ", err)
 	}
 	order.LAST_ASSIGNED_ID = -1
 }
