1c7a570f7a6013d7cedb80d32af7773d90f4026f
Florian Schlegel <florian.schlegel@bestbytes.com>
2016-05-13 14:16:58 +0200

    added some helpers

--- order/position.go
+++ order/position.go
@@ -10,5 +10,15 @@ type Position struct {
 	QuantityUnit string
 	Price        float64
 	IsATPApplied bool
+	Refund       bool
 	Custom       interface{}
+}
+
+func (p *Position) IsRefund() bool {
+	return p.Refund
+}
+
+// GetAmount returns the Price Sum of the position
+func (p *Position) GetAmount() float64 {
+	return p.Price * p.Quantity
 }
