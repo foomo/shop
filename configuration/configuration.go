cf2f0ac871b5135f660067378ada35221e4428b9
Florian Schlegel <florian.schlegel@bestbytes.com>
2016-05-13 14:17:52 +0200

    added some constants

--- configuration/configuration.go
+++ configuration/configuration.go
@@ -1,15 +1,19 @@
 package configuration
 
 const (
-	MONGO_URL                          string = "mongodb://127.0.0.1/project-globus-services-stories"
-	MONGO_COLLECTION_SHOP_EVENT_LOG    string = "shop_event_log"
-	MONGO_COLLECTION_ORDERS            string = "orders"
-	MONGO_COLLECTION_MOCK_TRANSACTIONS string = "mock_trx" // TODO Remove this when no longer used
+	MONGO_URL                                              string = "mongodb://127.0.0.1/project-globus-services-stories"
+	MONGO_COLLECTION_SHOP_EVENT_LOG                        string = "shop_event_log"
+	MONGO_COLLECTION_ORDERS                                string = "orders"
+	MONGO_COLLECTION_MOCK_TRANSACTIONS                     string = "mock_trx"                 // Several TLOGS from globus-service-docs TODO Remove this when no longer used
+	MONGO_COLLECTION_MOCK_TRANSACTIONS_FOR_CASHREPORT_TEST string = "mock_trx_cashreport_test" // The one TLOG where we have a corresponding CashReport TODO Remove this when no longer used
+	MONGO_COLLECTION_MOCK_ITEMS                            string = "mock_items"               // TODO Remove this when no longer used
 
 	//MONGO_COLLECTION_QUEUE_TEST     string = "queue_test"
-	TestSAPServer = "https://vspid950.gmsap.migros.ch:8444" //  (goes to localhost and is then tunneled to sap via dev-server)
-	//TestServer = "https://127.0.0.1:8444"
-	TestSendMockRequest = "https://globus-shop-dev.bestbytes.net:443"
+	TestSAPServer        = "https://vspid950.gmsap.migros.ch:8444"        //  (goes to localhost and is then tunneled to sap via dev-server)
+	TestGAUTServer       = "https://autorisierungstestews.globus.ch:8445" //  (goes to localhost and is then tunneled to GAUT via dev-server)
+	TestCadeauCardServer = "https://cadeaucardtestews.globus.ch:8446"
+	TestScoringServer    = "https://preprodservices.crif-online.ch"
+	TestSendMockRequest  = "https://globus-shop-dev.bestbytes.net:443"
 )
 
 var ORDER_ID_RANGE = []int{9200001, 9299999}
