f38d1f490a3ec1b92e416280a40caa47d97b96d8
Florian Schlegel <florian.schlegel@bestbytes.com>
2016-05-26 20:51:23 +0200

    finally removed server configs that didn't belong here

--- configuration/configuration.go
+++ configuration/configuration.go
@@ -12,20 +12,4 @@ const (
 	MONGO_COLLECTION_MOCK_TRANSACTIONS_FOR_CASHREPORT_TEST string = "mock_trx_cashreport_test" // The one TLOG where we have a corresponding CashReport TODO Remove this when no longer used
 	MONGO_COLLECTION_MOCK_ITEMS                            string = "mock_items"               // TODO Remove this when no longer used
 
-	//MONGO_COLLECTION_QUEUE_TEST     string = "queue_test"
-	TestSAPServer        = "https://vspid950.gmsap.migros.ch:8444"        //  (goes to localhost and is then tunneled to sap via dev-server)
-	TestGAUTServer       = "https://autorisierungstestews.globus.ch:8445" //  (goes to localhost and is then tunneled to GAUT via dev-server)
-	TestCadeauCardServer = "https://cadeaucardtestews.globus.ch:8446"
-	TestScoringServer    = "https://preprodservices.crif-online.ch"
-	TestSendMockRequest  = "https://globus-shop-dev.bestbytes.net:443"
 )
-
-var ORDER_ID_RANGE = []int{9200001, 9299999}
-
-//type StaticConfiguration struct {
-//	ShippingProvider  string
-//	SalesOrganistaion string
-//	SalesOffice       string
-//	SalesChannel      string
-//	CurrencyCode      string
-//}
