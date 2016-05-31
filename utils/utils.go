4096580a78313ca752be8e30b6e320b769259c7d
Florian Schlegel <florian.schlegel@bestbytes.com>
2016-05-31 16:30:54 +0200

    moved WhoCalledMe methods and added IteString

--- utils/utils.go
+++ utils/utils.go
@@ -73,27 +73,11 @@ func OpenInBrowser(name string, html string) error {
 
 }
 
-// WhoCalledMe returns a string identifying the caller of the function calling WhoCalledMe
-func WhoCalledMe() string {
-	return whoCalledMeSkip(5) + " => " + whoCalledMeSkip(4) + " => " + whoCalledMeSkip(3)
-}
-
-func whoCalledMeSkip(skip int) string {
-	// we get the callers as uintptrs - but we just need 1
-	fpcs := make([]uintptr, 1)
-
-	// skip 3 levels to get to the caller of whoever called Caller()
-	n := runtime.Callers(skip, fpcs)
-	if n == 0 {
-		return "n/a" // proper error her would be better
+// Ite => if then else. If true returns thenDo else elseDo
+func IteString(condition bool, thenDo string, elseDo string) string {
+	if condition {
+		return thenDo
+	} else {
+		return elseDo
 	}
-
-	// get the info of the actual function that's in the pointer
-	fun := runtime.FuncForPC(fpcs[0] - 1)
-	if fun == nil {
-		return "n/a"
-	}
-
-	// return its name
-	return fun.Name()
 }
