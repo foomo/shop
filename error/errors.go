4cfb577b0d9178e723aafc8e873e914a20903384
Florian Schlegel <florian.schlegel@bestbytes.com>
2016-06-01 17:08:13 +0200

    created error package and added functionality to determine if an error is of a specific kind

---
+++ error/errors.go
@@ -0,0 +1,16 @@
+package error
+
+import "strings"
+
+const (
+	ErrorNotFound     = "not found"
+	ErrorNotAvailable = "Not available"
+)
+
+// ErrorIs returns true, if err is of kind e, else false
+func ErrorIs(err error, e string) bool {
+	if err == nil {
+		return false
+	}
+	return strings.HasPrefix(err.Error(), e)
+}
---
+++ error/erros_test.go
@@ -0,0 +1,16 @@
+package error
+
+import (
+	"errors"
+	"testing"
+)
+
+func TestError(t *testing.T) {
+	err := errors.New("not found")
+	if !ErrorIs(err, ErrorNotFound) {
+		t.Fail()
+	}
+	if ErrorIs(err, "ErrorBla") {
+		t.Fail()
+	}
+}
