da1a238532f9d745522c15663ccd3d31cfad3645
Florian Schlegel <florian.schlegel@bestbytes.com>
2016-07-13 19:03:18 +0200

    new mongo url

--- configuration/configuration.go
+++ configuration/configuration.go
@@ -1,8 +1,10 @@
 package configuration
 
 const (
-	MONGO_DB                           string = "project-globus-services-stories"
-	MONGO_URL                          string = "mongodb://127.0.0.1/project-globus-services-stories"
+	//MONGO_DB string = "project-globus-services-stories"
+	//MONGO_URL string = "mongodb://dockerhost/project-globus-services-stories" // local
+	MONGO_URL string = "mongodb://mongo/project-globus-services-1" // with docker
+	//MONGO_URL                          string = "mongodb://mongo/project-globus-services-stories" // with docker
 	MONGO_COLLECTION_CREDENTIALS       string = "credentials"
 	MONGO_COLLECTION_SHOP_EVENT_LOG    string = "shop_event_log"
 	MONGO_COLLECTION_ORDERS            string = "orders"
