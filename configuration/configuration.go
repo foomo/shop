076069d9184c2d860bc617fafbb01b4ce81a3a4e
Florian Schlegel <florian.schlegel@bestbytes.com>
2016-07-18 14:41:53 +0200

    minor refactoring

--- configuration/configuration.go
+++ configuration/configuration.go
@@ -1,14 +1,13 @@
 package configuration
 
 const (
-	//MONGO_DB string = "project-globus-services-stories"
-	//MONGO_URL string = "mongodb://dockerhost/project-globus-services-stories" // local
-	MONGO_URL string = "mongodb://mongo/project-globus-services-1" // with docker
-	//MONGO_URL                          string = "mongodb://mongo/project-globus-services-stories" // with docker
+	MONGO_DB  string = "project-globus-services-stories"
+	MONGO_URL string = "mongodb://dockerhost/" + MONGO_DB // local
+	//MONGO_URL string = "mongodb://mongo/project-globus-services-1" // with docker
 	MONGO_COLLECTION_CREDENTIALS       string = "credentials"
 	MONGO_COLLECTION_SHOP_EVENT_LOG    string = "shop_event_log"
 	MONGO_COLLECTION_ORDERS            string = "orders"
 	MONGO_COLLECTION_ORDERS_HISTORY    string = "orders_history"
 	MONGO_COLLECTION_CUSTOMERS_HISTORY string = "customers_history"
-	MONGO_COLLECTION_CUSTOMERS         string = "customers" // TODO Remove this when no longer used
+	MONGO_COLLECTION_CUSTOMERS         string = "customers"
 )
