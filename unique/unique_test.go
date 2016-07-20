4d2e4d7f565db7f9c247800665c4bade81c196bb
Florian Schlegel <florian.schlegel@bestbytes.com>
2016-07-20 16:12:12 +0200

    Added test for concurrent generating of unique ID's.

--- unique/unique_test.go
+++ unique/unique_test.go
@@ -2,12 +2,43 @@ package unique
 
 import (
 	"fmt"
+	"log"
+	"sync"
 	"testing"
 )
 
-func TestCreateUniqueIds(t *testing.T) {
+func TestCreateUniqueIdsSimple(t *testing.T) {
 	for i := 0; i < 100; i++ {
 		newId := GetNewID()
 		fmt.Println(newId)
 	}
+}
+func TestCreateUniqueIdsConcurrent(t *testing.T) {
+	mutex := &sync.Mutex{}
+	generatedIds := map[string]bool{}
+	generate := func(id string, wg *sync.WaitGroup) {
+		defer wg.Done()
+		for i := 0; i < 1000; i++ {
+			newId := GetNewID()
+			fmt.Println("Generator: "+id, newId)
+			mutex.Lock()
+			_, ok := generatedIds[newId]
+			if ok {
+				// This id is not unique!
+				t.Fatal("Generated duplicate for id", newId)
+			}
+			generatedIds[newId] = true
+			mutex.Unlock()
+		}
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(4)
+	go generate("1", &wg)
+	go generate("2", &wg)
+	go generate("3", &wg)
+	go generate("4", &wg)
+	wg.Wait()
+	log.Println("All go routines finished")
+
 }
