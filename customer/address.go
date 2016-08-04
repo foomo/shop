1419467cff01fc17ba7324dd663b86514217a730
Florian Schlegel <florian.schlegel@bestbytes.com>
2016-08-04 15:19:54 +0200

    added HasScore() and Equals() on address

--- customer/address.go
+++ customer/address.go
@@ -3,7 +3,6 @@ package customer
 type Address struct {
 	Id                       string // is automatically set on AddAddress()
 	Person                   *Person
-	IsPrimary                bool // first address which is added to customer is prinmary (this can be changed later).
 	IsDefaultBillingAddress  bool
 	IsDefaultShippingAddress bool
 	Street                   string
@@ -15,10 +14,37 @@ type Address struct {
 	Department               string
 	Building                 string
 	PostOfficeBox            string
-	Trusted                  bool
+	Score                    *Score
 	Custom                   interface{}
 }
 
 func (address *Address) GetID() string {
 	return address.Id
+}
+
+type Score struct {
+	Trusted          bool
+	DateOfTrustCheck string
+}
+
+func (address *Address) HasScore() bool {
+	if address.Score == nil || address.Score.DateOfTrustCheck == "" {
+		return false
+	}
+	return true
+}
+
+// Equals checks if this is BASICALLY the same address. Custom, IsDefaultBilling/Shipping may be different
+func (address *Address) Equals(otherAddress *Address) bool {
+	equal := true
+
+	equal = equal && address.Person.FirstName == otherAddress.Person.FirstName
+	equal = equal && address.Person.LastName == otherAddress.Person.LastName
+	equal = equal && address.Street == otherAddress.Street
+	equal = equal && address.StreetNumber == otherAddress.StreetNumber
+	equal = equal && address.ZIP == otherAddress.ZIP
+	equal = equal && address.City == otherAddress.City
+	equal = equal && address.Country == otherAddress.Country
+
+	return equal
 }
