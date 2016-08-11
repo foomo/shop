9c656def11e6928e52d0e71b7e2ade2ac481eb21
Florian Schlegel <florian.schlegel@bestbytes.com>
2016-08-11 14:13:01 +0200

    added consts for docker and local configuration

--- configuration/configuration.go
+++ configuration/configuration.go
@@ -3,8 +3,11 @@ package configuration
 const (
 	MONGO_DB string = "project-globus-services-stories"
 
-	//MONGO_URL string = "mongodb://dockerhost/" + MONGO_DB //  (for unit tests)
-	MONGO_URL                          string = "mongodb://mongo/project-globus-services-1" // with docker
+	LocalUnitTests = "dockerhost/" + MONGO_DB
+	WithDocker     = "mongo/project-globus-services-1"
+
+	//MONGO_URL string = "mongodb://" + LocalUnitTests
+	MONGO_URL                          string = "mongodb://" + WithDocker
 	MONGO_COLLECTION_CREDENTIALS       string = "credentials"
 	MONGO_COLLECTION_SHOP_EVENT_LOG    string = "shop_event_log"
 	MONGO_COLLECTION_ORDERS            string = "orders"
