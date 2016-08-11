ce8e187296244ba24c1cdac62676d31f225b76a8
Florian Schlegel <florian.schlegel@bestbytes.com>
2016-08-11 14:11:08 +0200

    fixed error handling

--- test_utils/utils.go
+++ test_utils/utils.go
@@ -17,15 +17,27 @@ func GetTestUtilsDir() string {
 }
 
 // Drops order collection and event_log collection
-func DropAllCollections() {
-	cmd := exec.Command("mongo", "dockerhost/"+configuration.MONGO_DB, GetTestUtilsDir()+"/dropCollections.js")
+func DropAllCollections() error {
+	db := ""
+	if strings.Contains(configuration.MONGO_URL, configuration.LocalUnitTests) {
+		db = configuration.LocalUnitTests
+	} else {
+		db = configuration.WithDocker
+	}
+	cmd := exec.Command("mongo", db, GetTestUtilsDir()+"/dropCollections.js")
 	log.Println("Command.args: ", cmd.Args)
 	//cmd := exec.Command("mongo", "localhost:27017/"+foomo_shop_config.MONGO_DB, GetProjectDir()+"/mongo/dropCollections.js")
 	err := cmd.Start()
 	if err != nil {
 		log.Fatal(err)
+		return err
 	}
 	log.Printf("Waiting for command to finish...")
 	err = cmd.Wait()
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
 	log.Printf("Command finished with error: %v", err)
+	return nil
 }
