6b26ebccf4918d4e790f9cc5f7215d452097665a
Frederik Löffert <frederik@zitrusmedia.de>
2016-09-05 14:32:20 +0200

    allowed languages added to config

--- configuration/configuration.go
+++ configuration/configuration.go
@@ -1,13 +1,15 @@
 package configuration
 
 const (
-	MONGO_DB string = "project-globus-services-stories"
+	MONGO_DB      string = "shop"
+	MONGO_DB_TEST string = "project-globus-services-stories"
 
-	LocalUnitTests = "dockerhost/" + MONGO_DB
-	WithDocker     = "mongo/project-globus-services-1"
+	LocalUnitTests = "dockerhost/" + MONGO_DB_TEST
+	WithDocker     = "mongo/" + MONGO_DB
 
 	//MONGO_URL string = "mongodb://" + LocalUnitTests
-	MONGO_URL                          string = "mongodb://" + WithDocker
+	MONGO_URL string = "mongodb://" + WithDocker
+
 	MONGO_COLLECTION_CREDENTIALS       string = "credentials"
 	MONGO_COLLECTION_SHOP_EVENT_LOG    string = "shop_event_log"
 	MONGO_COLLECTION_ORDERS            string = "orders"
@@ -15,3 +17,6 @@ const (
 	MONGO_COLLECTION_CUSTOMERS_HISTORY string = "customers_history"
 	MONGO_COLLECTION_CUSTOMERS         string = "customers"
 )
+
+// AllowedLanguages contains language codes for all allowed languages
+var AllowedLanguages = [...]string{"de", "fr"}
--- test_utils/utils.go
+++ test_utils/utils.go
@@ -2,10 +2,11 @@ package test_utils
 
 import (
 	"log"
-	"os/exec"
 	"path"
 	"runtime"
 	"strings"
+
+	"gopkg.in/mgo.v2"
 
 	"github.com/foomo/shop/configuration"
 )
@@ -18,26 +19,37 @@ func GetTestUtilsDir() string {
 
 // Drops order collection and event_log collection
 func DropAllCollections() error {
-	db := ""
-	if strings.Contains(configuration.MONGO_URL, configuration.LocalUnitTests) {
-		db = configuration.LocalUnitTests
+	session, err := mgo.Dial(configuration.MONGO_URL)
+	if err != nil {
+		return err
+	}
+	defer session.Close()
+
+	log.Println("mongo session initialized", configuration.MONGO_URL, session)
+
+	collections, err := session.DB(configuration.MONGO_DB).CollectionNames()
+	if err != nil {
+		log.Println("unable to find CollectionNames", session)
 	} else {
-		db = configuration.WithDocker
+		log.Println("collections", collections)
 	}
-	cmd := exec.Command("mongo", db, GetTestUtilsDir()+"/dropCollections.js")
-	log.Println("Command.args: ", cmd.Args)
-	//cmd := exec.Command("mongo", "localhost:27017/"+foomo_shop_config.MONGO_DB, GetProjectDir()+"/mongo/dropCollections.js")
-	err := cmd.Start()
-	if err != nil {
-		log.Fatal(err)
-		return err
+
+	for _, collectionName := range collections {
+		collection := session.DB(configuration.MONGO_DB).C(collectionName)
+		count, err := collection.Count()
+
+		if err != nil {
+			log.Println("failed to find docs:", collectionName)
+		}
+
+		err = collection.DropCollection()
+		if err != nil {
+			log.Println("failed to drop collection:", collectionName, collection)
+		} else {
+			log.Printf("dropped collection %s with %d docs", collectionName, count)
+		}
 	}
-	log.Printf("Waiting for command to finish...")
-	err = cmd.Wait()
-	if err != nil {
-		log.Fatal(err)
-		return err
-	}
-	log.Printf("Command finished with error: %v", err)
+
+	log.Println("DropAllCollections finished")
 	return nil
 }
