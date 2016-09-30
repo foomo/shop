4f9a64d6661a4ca0130a00e8a1f6d8b33cdbfb66
Florian Schlegel <florian.schlegel@bestbytes.com>
2016-09-30 14:14:16 +0200

    refactored DropAllCollections()

--- test_utils/utils.go
+++ test_utils/utils.go
@@ -27,16 +27,28 @@ func GetTestUtilsDir() string {
 }
 
 // Drops order collection and event_log collection
+
 func DropAllCollections() error {
-	session, err := mgo.Dial(configuration.MONGO_URL)
+	err := DropAllCollectionsFromUrl(configuration.MONGO_URL, configuration.MONGO_DB)
+	if err != nil {
+		return err
+	}
+	err = DropAllCollectionsFromUrl(configuration.MONGO_URL_PRODUCTS, configuration.MONGO_DB_PRODUCTS)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+func DropAllCollectionsFromUrl(url string, db string) error {
+	session, err := mgo.Dial(url)
 	if err != nil {
 		return err
 	}
 	defer session.Close()
 
-	log.Println("mongo session initialized", configuration.MONGO_URL, session)
+	log.Println("mongo session initialized", url, session)
 
-	collections, err := session.DB(configuration.MONGO_DB).CollectionNames()
+	collections, err := session.DB(db).CollectionNames()
 	if err != nil {
 		log.Println("unable to find CollectionNames", session)
 	} else {
@@ -47,7 +59,7 @@ func DropAllCollections() error {
 		_, ok := NoDropList[collectionName]
 		// Only Drop Collections which are not on the no drop list
 		if !ok {
-			collection := session.DB(configuration.MONGO_DB).C(collectionName)
+			collection := session.DB(db).C(collectionName)
 			count, err := collection.Count()
 
 			if err != nil {
