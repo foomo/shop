ab87dc9201b3a3a531b7360857042c33c41d51b7
Florian Schlegel <florian.schlegel@bestbytes.com>
2016-10-21 16:39:33 +0200

    added new test

---
+++ pricerule/pricerule_globus_test.go
@@ -0,0 +1,48 @@
+package pricerule
+
+func createPriceRule() {
+
+	CreateProductGroup()
+	priceRule := NewPriceRule("ruleID")
+	priceRule.Name = map[string]string{
+		"de": "Rulename",
+		"fr": "Rulename",
+		"it": "Rulename",
+	}
+
+	priceRule.Description = map[string]string{
+		"de": "Description",
+		"fr": "Description",
+		"it": "Description",
+	}
+	priceRule.Type = TypePromotion
+
+	priceRule.Action = ActionItemByAbsolute
+
+	priceRule.Amount = 10.0
+
+	priceRule.Priority = 10
+    priceRule.CustomerActionType "402"
+    priceRule.CustomerPromoID ="5273639"
+	priceRule.IncludedProductGroupIDS = []string{"SaleProducts"}
+
+	// priceRule.IncludedCustomerGroupIDS = []string{CustomerGroupID1}
+
+	err := priceRule.Upsert()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func CreateProductGroup() {
+	group := new(Group)
+	group.Type = ProductGroup
+	group.ID = "SaleProducts"
+	group.Name = "SaleProducts"
+	group.AddGroupItemIDs([]string{"12387678"})
+	err := group.Upsert()
+	if err != nil {
+		t.Fatal("Could not upsert product group " + groupID)
+	}
+
+}
