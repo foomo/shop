43885cf66d418a5cf027088735ab63b52d09bec9
Florian Schlegel <florian.schlegel@bestbytes.com>
2016-11-05 11:00:42 +0100

    removed collection tlog_transactions form NoDropList

--- test_utils/utils.go
+++ test_utils/utils.go
@@ -18,7 +18,6 @@ var NoDropList = map[string]bool{
 	"mock_trx_cashreport_test": true,
 	"orders_many":              true,
 	"status_updates_many":      true,
-	"tlog_transactions":        true,
 }
 
 func GetTestUtilsDir() string {
