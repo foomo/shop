603eb22cc81e44624864be6b620c5eba97176d6f
Florian Schlegel <florian.schlegel@bestbytes.com>
2019-07-25 13:38:45 +0200

    Pricerules: add methodes to remove pricerules by query

--- pricerule/pricerule.go
+++ pricerule/pricerule.go
@@ -341,6 +341,14 @@ func (pricerule *PriceRule) Delete() error {
 	return err
 }
 
+func DeletePricesRules(query bson.M) error {
+	session, collection := GetPersistorForObject(new(PriceRule)).GetCollection()
+	defer session.Close()
+
+	_, err := collection.RemoveAll(query)
+	return err
+}
+
 // DeletePriceRule - delete PriceRule
 func DeletePriceRule(ID string) error {
 	session, collection := GetPersistorForObject(new(PriceRule)).GetCollection()
