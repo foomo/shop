85df3156086d7b4f107e4f1c7ffe0040530fb544
Florian Schlegel <florian.schlegel@bestbytes.com>
2019-11-20 15:01:45 +0100

    add TestForceUpsert

--- order/order_test.go
+++ order/order_test.go
@@ -3,7 +3,75 @@ package order
 import (
 	"log"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
+
+func TestForceUpsert(t *testing.T) {
+
+	session, collection := GetOrderPersistor().GetCollection()
+	defer session.Close()
+
+	assert.NoError(t, collection.DropCollection(), "clean up")
+
+	orderID := "Foo"
+	fnOrderID := func() (string, error) {
+		return orderID, nil
+	}
+
+	fnLog := func(t *testing.T, order *Order, comment string) {
+		t.Log()
+		t.Log(comment)
+		t.Log("id:", order.Id, "site:", order.Site)
+		t.Log("version previous:", order.Version.Previous, "current:", order.Version.Current)
+	}
+
+	// create order
+	order, err := NewOrderWithCustomId(nil, fnOrderID)
+	assert.NoError(t, err, "NewOrder")
+	fnLog(t, order, "new order")
+	// retrieve order from db
+	order, err = GetOrderById(orderID, nil)
+	assert.NoError(t, err, "GetOrderById")
+	fnLog(t, order, "load again")
+
+	// change order
+	order.Site = "globus"
+	assert.NoError(t, order.Upsert(), "upsert")
+	// get changed order
+	orderGlobus, err := GetOrderById(orderID, nil)
+	assert.NoError(t, err, "GetOrderById")
+	fnLog(t, orderGlobus, "load after upsert, to be used for later rollback")
+	// check if change is there
+	assert.Equal(t, "globus", orderGlobus.Site)
+
+	// change again
+	orderNavyBoot, err := GetOrderById(orderID, nil)
+	assert.NoError(t, err, "GetOrderById")
+	fnLog(t, orderNavyBoot, "load again")
+	orderNavyBoot.Site = "navyboot"
+	assert.NoError(t, orderNavyBoot.Upsert(), "upsert")
+
+	// load order again
+	orderNavyBoot, err = GetOrderById(orderID, nil)
+	assert.NoError(t, err, "GetOrderById")
+	fnLog(t, orderNavyBoot, "load after upsert")
+	// check if change is there
+	assert.Equal(t, "navyboot", orderNavyBoot.Site)
+
+	// rollback to orderGlobus
+	orderGlobus.SetForceUpsert(true)
+	fnLog(t, orderGlobus, "version of order used for rollback")
+	assert.NoError(t, orderGlobus.Upsert(), "upsert")
+
+	// check of site is globus again
+	orderGlobus, err = GetOrderById(orderID, nil)
+	assert.NoError(t, err, "GetOrderById")
+	fnLog(t, orderGlobus, "load after rollback")
+	// check if change is there
+	assert.Equal(t, "globus", orderGlobus.Site)
+
+}
 
 // Test transitions between states
 func TestOrderStatusTransition(t *testing.T) {
