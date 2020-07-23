e46ec90f504b08cb73aa4ff7e608971ee111d525
Florian Schlegel <florian.schlegel@bestbytes.com>
2020-07-23 11:55:40 +0200

    move Person VO to fix code generation

--- address/address.go
+++ address/address.go
@@ -99,3 +99,28 @@ func (address *Address) TrimSpace() {
 	address.Country = strings.TrimSpace(address.Country)
 	address.CountryCode = strings.TrimSpace(address.CountryCode)
 }
+
+// Person is a field Customer and of Address
+// Only Customer->Person has Contacts
+type Person struct {
+	FirstName       string
+	MiddleName      string
+	LastName        string
+	Title           TitleType
+	Salutation      SalutationType
+	Birthday        string
+	Contacts        map[string]*Contact    // key must be contactID
+	DefaultContacts map[ContactType]string // reference by contactID
+}
+
+func (person *Person) TrimSpace() {
+	if person == nil {
+		return
+	}
+	person.FirstName = strings.TrimSpace(person.FirstName)
+	person.MiddleName = strings.TrimSpace(person.MiddleName)
+	person.LastName = strings.TrimSpace(person.LastName)
+	person.Title = TitleType(strings.TrimSpace(string(person.Title)))
+	person.Salutation = SalutationType(strings.TrimSpace(string(person.Salutation)))
+	person.Birthday = strings.TrimSpace(person.Birthday)
+}
